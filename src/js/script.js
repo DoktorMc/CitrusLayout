60feb5d2cc96daaebb6a6b3bc2dd252b8394c17e
Виталий Матвиенко <matvienko_v@citrus.com.ua>
2020-06-19 18:34:47 +0300

    POST send

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,88 @@
-
+jQuery(function ($) {
+  $("#mobile").mask("+38(099) 999-99-99", { placeholder: "_" });
+});
 
 /* прикрепить событие submit к форме */
-$( document ).ready(function() {
-  let bonus = $(".bottom_bonus");
-  let thnks = $(".thnks_wrap");
 
-  if (bonus.is(":visible")) {
-    $("#bottom_bonus-form").submit(function(event) {
-  /* отключение стандартной отправки формы */
-  event.preventDefault();  
-  bonus.fadeIn(1000);
-    bonus.hide();
-    
-    thnks.show();
-    
-    
-  });
-  $("#bottom_thnks-button").click(function(event) {
-   if(thnks.css("display")==='block'){
-      event.preventDefault();
-      thnks.hide();
-      bonus.show();
-    }
-   });
-} 
+let bonus = $(".bottom_bonus");
+let thnks = $(".thnks_wrap");
 
-jQuery(function($){
-  $("#mobile").mask("+38(099) 999-99-99",{placeholder:"_"});
+
+$("#bottom_bonus-form").submit(function(e) {
+
+  e.preventDefault(); // avoid to execute the actual submit of the form.
+
+  var form = $(this);
+  var url = form.attr('action');
+
+  $.ajax({
+         type: "POST",
+         url: url,
+         data: $(form).serialize(), // serializes the form's elements.
+         success: function(data)
+         {
+             alert(data); // show response from the php script.
+         },
+         error: function () {
+          
+      },
+       });
+
+
 });
-});
 
-  
 
-  /* собираем данные с элементов страницы: */
-  // var $form = $( this ),
-  //     term = $form.find( 'input[name="phoneNumber"]' ).val(),
-  //     url = $form.attr( 'action' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#bottom_bonus-form").submit(function (event) {
+//   /* отключение стандартной отправки формы */
   
-  // /* отправляем данные методом POST */
-  // var posting = $.post( url, { phoneNumber: term } );
-  
-  /* результат помещаем в div */
-  // posting.done(function( data ) {
-    // var content = $( data ).find( '#content' );
-    // $( "#result" ).empty().append( content );
-  // });
+//     var $form = $('#bottom_bonus-form'),
+//       term = $form.find('input[name="phoneNumber"]').val(),
+//       url = $form.action;
+
+//     /* отправляем данные методом POST */
+//     var posting = $.post(url, { phoneNumber: term });
+//     posting.done(function () {
+//       if (bonus.is(":visible")) {
+//       event.preventDefault();
+//       bonus.fadeIn(1000);
+//       bonus.hide();
+
+//       thnks.show();
+
+
+//     });
+//     $("#bottom_thnks-button").click(function (event) {
+//       if (thnks.css("display") === 'block') {
+//         event.preventDefault();
+//         thnks.hide();
+//         bonus.show();
+//       }
+//     });
+//   }
+
+// });
+
+
+
+
+
 
